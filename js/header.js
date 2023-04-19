5cb7d2d46ff651fe2cbf02b6f5db4da31c5dee52
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-04-19 12:17:31 +0430

    add header file

---
+++ js/header.js
@@ -0,0 +1,38 @@
+const hamburger = document.querySelector(".nav_hamburger")
+const closeBtn = document.querySelector(".mobile_menu--close")
+const menu = document.querySelector(".nav_mobile--menu")
+
+
+hamburger.addEventListener("click" , ()=> {
+    menu.classList.add("show")
+    document.getElementById("body").style.backgroundColor="rgba(102,102,102,.4)";
+
+})
+
+
+closeBtn.addEventListener("click" , ()=> {
+    menu.classList.remove("show")
+    document.getElementById("body").style.backgroundColor="rgba(102,102,102,0)";
+})
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+  
+    document.getElementById("nav_logo--img").style.width ="50%";
+    document.getElementById("nav").style.backgroundColor="rgba(255,255,255,.9)";
+    document.getElementById("nav").style.padding="0";
+    document.getElementById("nav").style.height="62px";
+
+
+  } else {
+    document.getElementById("nav_logo--img").style.width ="80%";
+    document.getElementById("nav").style.backgroundColor="rgba(255,255,255,1)";
+    document.getElementById("nav").style.padding="0 0";
+    document.getElementById("nav").style.height="100px";
+    
+  }
+}
